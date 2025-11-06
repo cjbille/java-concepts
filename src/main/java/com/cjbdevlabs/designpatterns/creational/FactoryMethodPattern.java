ab70dec0d5880cbd0a2ba52fe1828611e75c6c6c
Chris Bille <cbille1@icloud.com>
2025-11-05 20:47:56 -0500

    Add FactoryMethodPattern

--- src/main/java/com/cjbdevlabs/designpatterns/creational/FactoryMethodPattern.java
+++ src/main/java/com/cjbdevlabs/designpatterns/creational/FactoryMethodPattern.java
@@ -6,9 +6,62 @@ of another class, but it doesn't know in advance what the type of concrete class
  */
 public class FactoryMethodPattern {
 
+    enum TicketType {
+        BUS,
+        TRAIN,
+        PLANE
+    }
+
     interface Ticket {
 
         int getPrice();
 
+    }
+
+    static class BusTicket implements Ticket {
+
+        @Override
+        public int getPrice() {
+            return 3;
+        }
+    }
+
+    static class TrainTicket implements Ticket {
+
+        @Override
+        public int getPrice() {
+            return 4;
+        }
+    }
+
+    static class PlaneTicket implements Ticket {
+
+        @Override
+        public int getPrice() {
+            return 40;
+        }
+    }
+
+    static class TicketMachine {
+
+        Ticket createTicket(TicketType ticketType) {
+            return switch (ticketType) {
+                case BUS -> new BusTicket();
+                case TRAIN -> new TrainTicket();
+                case PLANE -> new PlaneTicket();
+            };
+        }
+    }
+
+    static class TicketOffice {
+
+        private static final TicketMachine ticketMachine = new TicketMachine();
+
+        public static void main(String[] args) {
+            var busTicket = ticketMachine.createTicket(TicketType.BUS);
+            System.out.println("One bus ticket purchased. The price is $" + busTicket.getPrice() + ".");
+            var trainTicket = ticketMachine.createTicket(TicketType.TRAIN);
+            System.out.println("One train ticket purchased. The price is $" + trainTicket.getPrice() + ".");
+        }
     }
 }
